dc2ab89bbd5cdf11bcbacd1c397c6f7a3f39fa6b
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-12-10 12:22:20 +0100

    add implementation of MemberService

--- src/main/java/com/example/aftas/service/MemberService.java
+++ src/main/java/com/example/aftas/service/MemberService.java
@@ -7,11 +7,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public interface MemberService {
+public interface MemberService{
+
     Member save(Member member);
     List<Member> findAll();
     Optional<Member> findById(Long id);
-    Member update(Member memberUpdated);
+    Member update(Member memberUpdated, Long id);
     void delete(Long id);
 
 }
