39dd1b938c608861f6ae6898b778da84168f76da
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-12-12 20:01:38 +0100

    add fish & Level DTOs

--- src/main/java/com/example/aftas/dto/request/FishRequestDTO.java
+++ src/main/java/com/example/aftas/dto/request/FishRequestDTO.java
@@ -1,2 +1,27 @@
-package com.example.aftas.dto.request;public record FishRequestDTO() {
+package com.example.aftas.dto.request;
+
+import com.example.aftas.domain.Fish;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record FishRequestDTO(
+
+        @NotBlank
+        @NotNull(message = "the Fish name cannot be null")
+        @Size(min = 1, max = 50, message = "Name must be between 1 and 50 characters")
+        String name,
+
+        @Positive(message = "Average weight must be a positive number")
+        Double averageWeight
+
+) {
+
+    public Fish toFish(){
+        return Fish.builder()
+                .name(this.name)
+                .averageWeight(this.averageWeight)
+                .build();
+    }
 }
